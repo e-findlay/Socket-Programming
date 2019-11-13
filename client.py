7579ef8aa10fdd40866211dfcdf0cccb251ce1a1
Edmund Findlay <fox2edmund@gmail.com>
2019-11-13 21:23:59 +0000

    Updated POST Command

--- client.py
+++ client.py
@@ -1,21 +1,85 @@
 import socket
 import sys
-
+# get servername from cmd line argument
 if len(sys.argv) > 1:
     serverName = sys.argv[1]
 else:
     serverName = ''
+# get port number from cmd line argument
 if len(sys.argv) > 2:
     serverPort = int(sys.argv[2])
 else:
     serverPort = 8080
 print(serverName, serverPort)
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-message = 'GET_BOARD'
-clientSocket.send(message.encode())
-response = clientSocket.recv(1024)
-print(response)
-cmd = input()
-print(cmd)
-clientSocket.close()
+
+
+
+def GET_BOARDS(message):
+    # create client socket
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    message = message.encode()
+    clientSocket.send(message)
+    response = clientSocket.recv(1024).decode()
+    # handle error
+    if response == 'No message boards defined':
+        print(response)
+        clientSocket.close()
+    response = response.split('/')
+    print(response)
+    # print board names to command line
+    for i, j in enumerate(response[:-1]):
+        print('{}. '.format(i+1),j)
+    clientSocket.close()
+    return response
+
+
+def GET_MESSAGES(message):
+    # create client socket
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    msg = message.encode()
+    clientSocket.connect((serverName, serverPort))
+    clientSocket.send(msg)
+    response = clientSocket.recv(1024)
+    print(response.decode())
+    clientSocket.close()
+
+def POST_MESSAGE(message):
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    msg = message.encode()
+    clientSocket.send(msg)
+    response = clientSocket.recv(1024)
+    print(response.decode())
+    clientSocket.close()
+
+
+message = 'GET_BOARDS'
+response = GET_BOARDS(message)
+
+    # keep running until user enters QUIT
+while True:
+    cmd = str(input())
+    print(cmd)
+    if cmd in range(1, len(response)):
+        msg = 'GET_MESSAGES'
+        msg += '/' + response[int(cmd)]
+        GET_MESSAGES(msg)
+            
+    if cmd == 'POST':
+        msg = 'POST_MESSAGE/'
+        boardNumber = input('Which board would you like to post to? \n')
+        boardName = response[int(boardNumber) - 1]
+        msg += str(boardName)
+        msg_title = input('Enter your message title: \n')
+        print(msg_title == str)
+        msg += '/' + str(msg_title)
+        msg_content = input('Enter your message content: \n')
+        msg += '/' + str(msg_content)
+        POST_MESSAGE(str(msg))
+
+    if cmd == 'QUIT':
+        # break infinite loop
+        break
+print("QUIT successful")
+
--- server.py
+++ server.py
@@ -1,32 +1,102 @@
 import socket
 import sys
 import os
+from time import strftime, gmtime
+import threading
+
 
 print(sys.argv)
+# set hostname to command line input
 if len(sys.argv) > 1:
     hostname = sys.argv[1]
 else:
     hostname = ''
 print(hostname)
+# set port number to command line input
 if len(sys.argv) > 2:
        serverPort = int(sys.argv[2])
 else:
     serverPort = 8080
 print(serverPort)
+
+
+def getMessages(msg):
+    msg = msg.split('/')
+    boardName = msg[1]
+    filePath = '/boards/' + boardName
+    files = ''
+    for f in os.listdir(filePath):
+        if os.path.isfile(os.path.join(filePath, f)):
+            files += f + '/'
+    return files
+
+def postMessage(msg):
+    msg = msg.split('/')
+    print(msg)
+    boardName = msg[1]
+    print(boardName)
+    messageTitle = msg[2]
+    messageContents = msg[3]
+    filename = strftime('%Y%m%d-%H%M%S-' + messageTitle, gmtime())
+    print(filename)
+    
+    boardPath = os.path.dirname('board/' + boardName)
+    print(boardPath)
+    msgboardPath = os.path.join(boardPath, boardName)
+    filePath = os.path.join(msgboardPath, filename)
+    print(filePath)
+    try:
+        f = open(filePath, 'w')
+        f.write(messageContents)
+        f.close()
+        reply = "File Created"
+    except:
+        reply = "Error Creating File"
+    return reply
+
+
+def getBoards():
+        # search boards directory for message boards
+        boards = os.listdir('./board')
+        reply = ''
+        for board in boards:
+            reply = board + '/'
+        return reply[:-1]
+
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind((hostname, serverPort))
-serverSocket.listen(1)
-print('Server Created')
+try:
+    serverSocket.bind((hostname, serverPort))
+    serverSocket.listen(5)
+except Exception as e:
+    print("Could not bind server error: {}", e)
+
+def clientThread(connection, address):
+    ip = address[0]
+    port = address[1]
+    print(f"Connection made from IP: {ip} and port: {port}")
+    while True:
+        msg = connection.recv(1024).decode()
+        print(msg)
+        if msg == 'GET_BOARDS':
+            reply = getBoards()
+        elif msg[:12] == 'GET_MESSAGES':
+            reply = getMessages(msg)
+        elif msg[:12] == 'POST_MESSAGE':
+            reply = postMessage(msg)
+        else:
+            print(msg[:12])
+            reply = "Error"
+        if not msg:
+            break
+        print(msg)
+        connection.sendall(reply.encode())
+    connection.close()
 while True:
-    connectionSocket, address = serverSocket.accept()
-    message = connectionSocket.recv(1024).decode()
-    if message == 'GET_BOARD':
-        message = os.listdir('./board')
-        connectionSocket.send(message)
-    elif message == 'POST_MESSAGES':
-        pass
-    elif message == 'POST_MESSAGE':
-        pass
-    else:
-        connectionSocket.send('Error: Command not recognised'.encode())
-    connectionSocket.close()
+    try:
+        conn, addr = serverSocket.accept()
+        threading._start_new_thread(clientThread, (conn, addr))
+    except Exception as e:
+        print("Error: {}".format(e))
+serverSocket.close()
+        
+
