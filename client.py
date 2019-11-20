9a534c7becdc577e56822fd188ee1825b7cfc71d
Edmund Findlay <fox2edmund@gmail.com>
2019-11-20 13:15:21 +0000

    Updated client and server files for error handling

--- client.py
+++ client.py
@@ -17,19 +17,36 @@ print(serverName, serverPort)
 def GET_BOARDS(message):
     # create client socket
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
+    clientSocket.settimeout(10)
+    # attempt to connect to server
+    try:
+        clientSocket.connect((serverName, serverPort))
+    # print error and quit program if connection unsuccessful
+    except:
+        print('Server not available')
+        sys.exit()
     message = message.encode()
+    # send command to server
     clientSocket.send(message)
-    response = clientSocket.recv(1024).decode()
-    # handle error
+    # decode response
+    try:
+        response = clientSocket.recv(1024).decode()
+    except socket.timeout:
+        print('Timeout Error')
+        clientSocket.close()
+        sys.exit()
+    # handle error and quit program
     if response == 'No message boards defined':
         print(response)
         clientSocket.close()
+        sys.exit()
+    # convert response to array of message boards
     response = response.split('/')
     print(response)
     # print board names to command line
-    for i, j in enumerate(response[:-1]):
-        print('{}. '.format(i+1),j)
+    for i, j in enumerate(response):
+        print('{}. {}'.format(i+1,j))
+    # close connection
     clientSocket.close()
     return response
 
@@ -37,49 +54,89 @@ def GET_BOARDS(message):
 def GET_MESSAGES(message):
     # create client socket
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.settimeout(10)
     msg = message.encode()
-    clientSocket.connect((serverName, serverPort))
+    # attempt to connect to server
+    try:
+        clientSocket.connect((serverName, serverPort))
+    # print error and quit program if connection unsuccessful
+    except:
+        print('Server not available')
+        sys.exit()
     clientSocket.send(msg)
-    response = clientSocket.recv(1024)
-    print(response.decode())
+    try:
+        response = clientSocket.recv(1024)
+    except socket.timeout as error:
+        print('Timeout error')
+        clientSocket.close()
+        sys.exit()
+    response = response.decode()
+    # separate response into arrays for message titles and message content
+    messages = response.split('//')
+    # create array of titles
+    messageTitles = messages[0].split('/')
+    # create array of contents
+    messageContent = messages[1].split('/')
+    # print title and contents for each message to command line
+    for i in range(len(messageTitles)):
+        print(f'{messageTitles[i]}: \n{messageContent[i]}\n')
+    # close connection
     clientSocket.close()
 
 def POST_MESSAGE(message):
+    # create client socket
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
+    clientSocket.settimeout(10)
+    # attempt to connect to server
+    try:
+        clientSocket.connect((serverName, serverPort))
+    # print error and quit program if connection unsuccessful
+    except:
+        print('Server not available')
+        sys.exit()
     msg = message.encode()
     clientSocket.send(msg)
-    response = clientSocket.recv(1024)
+    try:
+        response = clientSocket.recv(1024)
+    except socket.timeout:
+        print('Timeout Error')
+        clientSocket.close()
+        sys.exit()
     print(response.decode())
+    # close connection
     clientSocket.close()
 
 
 message = 'GET_BOARDS'
+# send message to server to get list of message board names
 response = GET_BOARDS(message)
 
-    # keep running until user enters QUIT
+# keep running until user enters QUIT
 while True:
-    cmd = str(input())
+    # wait for used input
+    cmd = input()
     print(cmd)
-    if cmd in range(1, len(response)):
+    if cmd in str(range(1, len(response)+1)):
         msg = 'GET_MESSAGES'
-        msg += '/' + response[int(cmd)]
+        msg += '/' + response[int(cmd)-1]
         GET_MESSAGES(msg)
-            
+        
     if cmd == 'POST':
         msg = 'POST_MESSAGE/'
         boardNumber = input('Which board would you like to post to? \n')
         boardName = response[int(boardNumber) - 1]
         msg += str(boardName)
-        msg_title = input('Enter your message title: \n')
-        print(msg_title == str)
-        msg += '/' + str(msg_title)
-        msg_content = input('Enter your message content: \n')
-        msg += '/' + str(msg_content)
+        msgTitle = input('Enter your message title: \n')
+        print(msgTitle == str)
+        msg += '/' + str(msgTitle)
+        msgContent = input('Enter your message content: \n')
+        msg += '/' + str(msgContent)
         POST_MESSAGE(str(msg))
 
     if cmd == 'QUIT':
         # break infinite loop
         break
+
+        
 print("QUIT successful")
 
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import socket
 import sys
 import os
+import time
 from time import strftime, gmtime
+from datetime import datetime
 import threading
 
 
@@ -23,12 +25,24 @@ print(serverPort)
 def getMessages(msg):
     msg = msg.split('/')
     boardName = msg[1]
-    filePath = '/boards/' + boardName
-    files = ''
-    for f in os.listdir(filePath):
+    filePath = 'board/' + boardName
+    filecontent = ''
+    filenames = ''
+    files = sorted(os.listdir(filePath))
+    print(files)
+    if len(files) > 100:
+        files = files[:100]
+    
+    for f in files:
+        print(f)
         if os.path.isfile(os.path.join(filePath, f)):
-            files += f + '/'
-    return files
+            filenames += f + '/'
+            with open(os.path.join(filePath, f)) as file:
+                content = file.read()
+                filecontent += content + '/'
+    reply = filenames + '//' + filecontent
+    print(reply)
+    return reply
 
 def postMessage(msg):
     msg = msg.split('/')
@@ -59,44 +73,81 @@ def getBoards():
         # search boards directory for message boards
         boards = os.listdir('./board')
         reply = ''
+        # if no message boards defined, print error and quit program
+        if len(boards) == 0:
+            print('No message boards defined')
+            sys.exit()
         for board in boards:
-            reply = board + '/'
+            if not os.path.isfile(os.path.join('board', board)):
+                reply += board + '/'
         return reply[:-1]
 
+# create server socket
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# attempt to bind socket to port
 try:
     serverSocket.bind((hostname, serverPort))
     serverSocket.listen(5)
+# print error and quit program if port busy
 except Exception as e:
-    print("Could not bind server error: {}", e)
+    print("Busy port: {}".format(e))
+    sys.exit()
 
 def clientThread(connection, address):
     ip = address[0]
     port = address[1]
+    logData = f"{ip}:{port}"
+    # get current time
+    logDate = datetime.now().strftime('%d %B %H:%M:%S %Y')
+    # concatenate current time to log data
+    logData += '\t' + logDate
     print(f"Connection made from IP: {ip} and port: {port}")
     while True:
         msg = connection.recv(1024).decode()
         print(msg)
         if msg == 'GET_BOARDS':
+            logData += '\t' + 'GET_BOARDS'
             reply = getBoards()
         elif msg[:12] == 'GET_MESSAGES':
+            logData += '\t' + 'GET_MESSAGES'
             reply = getMessages(msg)
         elif msg[:12] == 'POST_MESSAGE':
+            logData += '\t' + 'POST_MESSAGE'
             reply = postMessage(msg)
         else:
+            logData += '\t' + 'UNKNOWN'
             print(msg[:12])
-            reply = "Error"
+            reply = "Invalid message"
         if not msg:
             break
         print(msg)
-        connection.sendall(reply.encode())
+        try:
+            connection.sendall(reply.encode())
+            logData += '\t' + 'OK'
+        except:
+            logData += '\t' + 'Error\n'
+        # open server.log in append mode if already exists
+        if os.path.exists('server.log'):
+            mode = 'a'
+        # open server.log in write mode if doesn't exist
+        else:
+            mode = 'w'
+        f = open('server.log', mode)
+        # write log data to server .log
+        f.write(logData)
+        # close file
+        f.close()
+    # close connection
     connection.close()
+
+# infinite loop to create thread for each new request
 while True:
     try:
         conn, addr = serverSocket.accept()
         threading._start_new_thread(clientThread, (conn, addr))
     except Exception as e:
         print("Error: {}".format(e))
+        break
 serverSocket.close()
         
 
